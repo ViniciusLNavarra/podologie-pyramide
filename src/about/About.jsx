c5f7b4acecd108d136a88e11173f384ab56f6257
Vinicius L. Navarra <viniciuslnavarra@outlook.com>
2023-06-07 10:58:03 +0200

    Changed import in About.jsx

--- src/about/About.jsx
+++ src/about/About.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import '../style/tailwind.css'
-import Navbar from '../Navbar'
-import Footer from '../Footer'
+import Navbar from '../navbar'
+import Footer from '../footer'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
